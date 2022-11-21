be621f17ac096c6741497fa0da93b2170091e738
Axel Pestoni <axelkpesto@gmail.com>
2022-11-20 22:18:29 -0800

    Up Arrow and More

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+let mybutton = document.querySelectorAll(".up-arrow");
+
+function scrollStatus() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function sendToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+  console.log("sent to top")
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
   if($navbarBurgers.length > 0) {
@@ -11,3 +27,5 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 });
+
+window.onscroll = function() {scrollStatus()};
